7fe7debf5efd40bd0b415e3416ce44c20228f493
Sergey Karpukhin <telegram.skdvp@gmail.com>
2021-01-20 01:09:57 +0300

    Раздел 7:Spring Security 93. Хранение пароля в БД в не шифрованном формате

--- src/main/java/com/zaurtregulov/spring/security/configuration/MyConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MyConfig.java
@@ -1,16 +1,28 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
 @Configuration
 @ComponentScan("com.zaurtregulov.spring.security")
+@PropertySource(value = "classpath:db.properties")
 @EnableWebMvc
 public class MyConfig {
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public ViewResolver viewResolver(){
@@ -20,6 +32,24 @@ public class MyConfig {
         internalResourceViewResolver.setSuffix(".jsp");
 
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass(env.getProperty("db.driver"));
+            dataSource.setJdbcUrl(env.getProperty("db.url"));
+            dataSource.setUser(env.getProperty("db.username"));
+            dataSource.setPassword(env.getProperty("db.password"));
+
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+
+        return dataSource;
     }
 
 }
